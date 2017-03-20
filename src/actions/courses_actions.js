6d10be9579a3137fdc651af316e8958c84944302
Filip Larsson <filip.larsson.vbg@gmail.com>
2017-03-20 21:36:43 +0100

    Fixed fetching courses from DB

--- src/actions/courses_actions.js
+++ src/actions/courses_actions.js
@@ -5,7 +5,11 @@ export function fetchCourses() {
   return (dispatch, getState) => {
     const route = '/courseNames'
     return Api.get(route).then((resp) => {
-      console.log(resp)
+      //temp print, need to geet courses to the view.
+      for(var i = 0; i<resp.Course_Name.length; i++){
+        console.log(i + ": " + resp.Course_Name[i])
+      }
+
     }).catch( (err) => { console.log(err)})
   }
 }
--- src/lib/api/api.js
+++ src/lib/api/api.js
@@ -28,13 +28,8 @@ class Api {
     const url = `${host}${route}`
     let options = Object.assign({ method: verb }, params ? { body: JSON.stringify(params) } : null );
     options.headers = Api.headers()
-    return fetch(url, options).then( resp => {
-      let json = resp.json();
-      if (resp.ok) {
-        return json
-      }
-      return json.then(err => {throw err});
-    }).then( json => json.results );
+    
+    return fetch(url,options).then((response) => response.json())
   }
 }
 
