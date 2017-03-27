f84725d0ea64dba8d57982f365d12b7f5cbf1333
Erik <erikejan@hotmail.com>
2017-03-27 15:05:40 +0200

    Moved courses_* to remote_courses_*

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
-import * as CoursesActions from './courses_actions'
+import * as RemoteCoursesActions from './remote_courses_actions'
 
 export const ActionCreators = Object.assign({},
-  CoursesActions
+  RemoteCoursesActions
 )
 
