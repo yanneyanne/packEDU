5fb4e1670d76380018d7a1893607f88cc24d5646
Erik <erikejan@hotmail.com>
2017-03-28 21:24:15 +0200

    Latest downloaded course now being persistently saved, though multiple courses not being saved

--- src/reducers/courses_reducer.js
+++ src/reducers/courses_reducer.js
@@ -4,23 +4,20 @@ import { Map, List } from 'immutable'
 
 export const courses = createReducer(Map(), {
   [types.LOAD_LOCAL_COURSES] (state, action) {
-    //storage.load({
-    //  key: "58c152c4e65a3494544dc833"
-    //}).then(ret => {
-    //  console.log("Loading local course in reducer:")
-    //  console.log(ret)
-    //})
+    storage.load({
+      key: 'courses'
+    }).then(ret => {
+      console.log("Loading local courses in reducer:")
+      console.log(ret)
+    }).catch( (err) => {console.log(err)})
     return state
   },
+
   [types.DOWNLOAD_REMOTE_COURSE](state, action) {
-    console.log("Firing in local course reducer") 
-    storage.save({
-      key: action.course.id,
-      rawData: {
-        name: action.course.name,
-        material: action.course.material 
-      }
-    })
+    storage.setItem("courses", JSON.stringify({ //Storage requires a string
+        "id": action.course.id,
+        "rawData": action.course
+    })).catch( (err) => {console.log(err)})
     let courses = state
     const newCourses = state.set(action.course.id, action.course.name)
     return newCourses
