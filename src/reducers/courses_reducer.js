cde24c37ab620c82a3568e0664dd4c8e31bd2de8
Erik <erikejan@hotmail.com>
2017-03-29 08:15:21 +0200

    Multiple courses now being saved!

--- src/reducers/courses_reducer.js
+++ src/reducers/courses_reducer.js
@@ -1,12 +1,11 @@
 import createReducer from '../lib/createReducer'
 import * as types from '../actions/types'
 import { Map, List } from 'immutable'
+import { AsyncStorage } from 'react-native'
 
 export const courses = createReducer(Map(), {
   [types.LOAD_LOCAL_COURSES] (state, action) {
-    storage.load({
-      key: 'courses'
-    }).then(ret => {
+    AsyncStorage.getItem("courses").then(ret => {
       console.log("Loading local courses in reducer:")
       console.log(ret)
     }).catch( (err) => {console.log(err)})
@@ -14,10 +13,17 @@ export const courses = createReducer(Map(), {
   },
 
   [types.DOWNLOAD_REMOTE_COURSE](state, action) {
-    storage.setItem("courses", JSON.stringify({ //Storage requires a string
-        "id": action.course.id,
-        "rawData": action.course
-    })).catch( (err) => {console.log(err)})
+    let courseId = action.course.id
+    let courseName = action.course.name
+    let courseMaterial = action.course.material
+   // AsyncStorage.removeItem("courses")
+    let courseObj = {}
+    courseObj[courseId] = {
+      name: courseName,
+      material: courseMaterial
+    }
+    AsyncStorage.mergeItem("courses", JSON.stringify(courseObj)
+    ).catch( (err) => {console.log(err)})
     let courses = state
     const newCourses = state.set(action.course.id, action.course.name)
     return newCourses
