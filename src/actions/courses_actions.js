58440251daf225d31f3a2f1147ac137e261d3ddf
Erik <erikejan@hotmail.com>
2017-04-11 10:26:15 +0200

    Correctly identifying needed evaluators

--- src/actions/courses_actions.js
+++ src/actions/courses_actions.js
@@ -25,11 +25,41 @@ export function downloadRemoteCourse(courseId) {
   return (dispatch, getState) => {
     const route = '/courseMaterial/' + courseId
     return Api.get(route).then((resp) => {
+      retrieveEvaluators(resp.material)
+      while(true){}
       dispatch(dispatchDownloadRemoteCourse({
         course: resp
       }))
     }).catch( (err) => { console.log(err) })
   }
+}
+
+function retrieveEvaluators(material) {
+  let ids = identifyEvaluators(material)
+}
+
+function identifyEvaluators(material) {
+  //Loop through the material looking for evaluators
+  let idx = 0
+  let evaluators = []
+  let searchTerm = "evaluator"
+  let alphanumeric = /\w+/
+  while (idx != -1) {
+    idx = material.indexOf(searchTerm, idx)
+    if (idx != -1) {
+      idx += searchTerm.length
+      while (!alphanumeric.test(material.charAt(idx)) && idx < material.length) {
+        idx++
+      }
+      let keyEnd = idx+1
+      while (alphanumeric.test(material.charAt(keyEnd)) && keyEnd < material.length) {
+        keyEnd++
+      }
+      evaluators.push(material.slice(idx, keyEnd))
+    }
+  }
+  console.log("These are the evaluators")
+  console.log(evaluators)
 }
 
 function dispatchDownloadRemoteCourse({ course }) {
