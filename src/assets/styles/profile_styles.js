0b59e45a8fc854b78b6c911b6b3d418efa103931
Erik <erikejan@hotmail.com>
2017-04-22 17:18:20 +0200

    Added basic styling to achievementscontainer

--- src/assets/styles/profile_styles.js
+++ src/assets/styles/profile_styles.js
@@ -1,7 +1,10 @@
 import { StyleSheet } from 'react-native';
 import { dimensions } from './constants'
+import general from './general_styles'
 
 var profileStyles = StyleSheet.create({
+	content:
+		general.content,
 
 })
 
