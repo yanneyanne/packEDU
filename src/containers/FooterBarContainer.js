5cbeb3cff2977e5db829addb4545a3d8070ce669
hgpc <herman.carlstrom@gmail.com>
2017-05-08 16:08:31 +0200

    Fixed styling to bottombar

--- src/containers/FooterBarContainer.js
+++ src/containers/FooterBarContainer.js
@@ -3,9 +3,15 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { ActionCreators } from '../actions'
 import { Actions } from 'react-native-router-flux'
-import { Footer, FooterTab, Button, Text, View } from 'native-base'
+import { Footer, FooterTab, Button, Text, View, Icon } from 'native-base'
 import Alignment from './AlignmentContainer'
 import * as language from '../assets/styles/language_strings'
+import homeImg from '../assets/imgs/home.png'
+import profileImg from '../assets/imgs/profile.png'
+import achievementsImg from '../assets/imgs/achievements.png'
+import downloadImg from '../assets/imgs/download.png'
+import { Image } from 'react-native'
+
 
 class FooterBar extends Component {
 
@@ -15,35 +21,28 @@ class FooterBar extends Component {
 
  render() {
     let homePage =
-      <Button active={this.isActiveTab("home")} onPress = {() => Actions.home()} key={"home"}>
-        <Text>
-          {this.props.getLanguage.home}
-        </Text>
+      <Button  active={this.isActiveTab("home")} onPress = {() => Actions.home()} key={"home"}>
+      <Image style={{width: 30, height: 30}}source = {homeImg} />
       </Button>
 
     let downloadPage =
       <Button active={this.isActiveTab("remotes")} onPress = {() => Actions.remotes()} key={"download"}>
-        <Text>
-          {this.props.getLanguage.download}
-        </Text>
+      <Image style={{width: 30, height: 30}}source = {downloadImg} />
       </Button>
 
     let achievementsPage =
       <Button active={this.isActiveTab("achievements")} onPress = {() => Actions.achievements()} key={"achievements"}>
-        <Text>
-          {this.props.getLanguage.achievements}
-        </Text>
+      <Image style={{width: 30, height: 30}}source = {achievementsImg} />
       </Button>
 
     let profilePage =
       <Button active={this.isActiveTab("profile")} onPress = {() => Actions.profile()} key={"profile"}>
-        <Text>
-          {this.props.getLanguage.profile}
-        </Text>
+      <Image style={{width: 30, height: 30}}source = {profileImg} />
       </Button>
 
     let pageList =[homePage, downloadPage, achievementsPage, profilePage]
     {this.props.settingsAlignRight ? pageList.reverse() : pageList}
+
     return (
       <View>
         {this.props.activeTab != "slide" && this.props.activeTab != "finishedLesson" ?
