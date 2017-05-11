8e5727e5bbe5685790f7651f1222fde43310fed6
Filip Larsson <filip.larsson.vbg@gmail.com>
2017-05-11 15:42:59 +0200

    Fixed finishedlesson styling to work on smallers screens

--- src/containers/FinishedLessonContainer.js
+++ src/containers/FinishedLessonContainer.js
@@ -11,13 +11,13 @@ import { Bar , Circle} from 'react-native-progress'
 import SCompile from '../lib/slideCompile/SCompile'
 import Alignment from './AlignmentContainer'
 import * as language from '../assets/styles/language_strings'
-import styles from '../assets/styles/slide_styles'
+import styles from '../assets/styles/finishedLesson_styles'
 import LinearGradient from 'react-native-linear-gradient'
 import { StyleSheet, Dimensions } from 'react-native'
 import { Map } from 'immutable'
 
 class FinishedLesson extends Component {
-  
+
   results() {
     let correctAnswers = 0
     let allAnswers = 0
@@ -45,26 +45,29 @@ class FinishedLesson extends Component {
       <LinearGradient colors={['#f4a791', '#f3818a']} style={styles.content}>
         <Bar progress={1} borderWidth={0} color={'rgba(255,255,255,1)'}
           width={width} borderRadius={0} style={styles.progress}/>
-        <View style={StyleSheet.flatten(styles.slideElements)}>
+        <View style={StyleSheet.flatten(styles.finishedElements)}>
           <SlideText>
-            Congratulations! You just completed 
+            Congratulations! You just completed
           </SlideText>
-          <Heading>
+          <SlideText>
             {this.props.activeLesson}
-          </Heading>
-          <Circle borderWidth = {0.5} size = {width/2} progress = {percentageAnswers} color={'rgba(255,255,255,1)'} style = {styles.progress} marginBottom={20}/>
-          <Heading>
-            {answers[1]} / {answers[0]}
-          </Heading>
+          </SlideText>
+          <Circle borderWidth = {0.5} size = {width/2} progress = {percentageAnswers} color={'rgba(255,255,255,1)'} marginBottom={20}/>
+          <View style={StyleSheet.flatten(styles.resultsContainer)}>
+            <Text style={StyleSheet.flatten(styles.resultsText)}>
+              {answers[1]} / {answers[0]}
+            </Text>
+          </View>
+
           <SlideText>
             Correct answers!
           </SlideText>
         </View>
         <View style={styles.footer}>
-          <View style = {StyleSheet.flatten([styles.nextPrevButtonsContainer], {flexDirection: flexDir})}>
-            <Button full bordered key={'done'} style={StyleSheet.flatten(styles.nextPrevButton)}
+          <View>
+            <Button full bordered key={'done'} style={StyleSheet.flatten(styles.chooseNewLessonButton)}
               onPress={() => Actions.lessons()}>
-              <Text style={StyleSheet.flatten(styles.nextPrevButtonText)}>
+              <Text style={StyleSheet.flatten(styles.finishedLessonButtonText)}>
                 Choose another lesson
               </Text>
             </Button>
